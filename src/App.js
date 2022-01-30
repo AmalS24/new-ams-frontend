6cd1eeef794b9da72e2052c2737c4cf672708c39
Amal <amalmsp64@gmail.com>
2022-01-30 16:04:30 +0530

    admin-page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import LoginForm from "./LoginForm";
 import NriForm from "./ams-client/NriForm";
 import RegisterForm from "./RegisterForm";
+import Home from "./ams-admin/Home";
 
 const App = () => {
   return (
     <>
       {/* <RegisterForm /> */}
       {/* <LoginForm /> */}
-      <NriForm />
+      {/* <NriForm /> */}
+      <Home />
     </>
   );
 };
